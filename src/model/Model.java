12fa339554406b99c502b81c9cd3619ecc5f426f
davidmarsal <davidmarsal@iesebre.com>
2020-11-10 15:56:03 +0100

    Sync

--- src/model/Model.java
+++ src/model/Model.java
@@ -10,5 +10,80 @@ package model;
  * @author profe
  */
 public class Model {
+
+    private String nomEquip;
+    private int golsEnContra;
+    private int golsAfavor;
+    private int partitsGuanyats;
+    private int partitsPerduts;
+    private int partitsEmpatats;
+    private int puntsEquip;
+    private int jornada;
+
+    public String getNomEquip() {
+        return nomEquip;
+    }
+
+    public void setNomEquip(String nomEquip) {
+        this.nomEquip = nomEquip;
+    }
+
+    public int getGolsEnContra() {
+        return golsEnContra;
+    }
+
+    public void setGolsEnContra(int golsEnContra) {
+        this.golsEnContra = golsEnContra;
+    }
+
+    public int getGolsAfavor() {
+        return golsAfavor;
+    }
+
+    public void setGolsAfavor(int golsAfavor) {
+        this.golsAfavor = golsAfavor;
+    }
+
+    public int getPartitsGuanyats() {
+        return partitsGuanyats;
+    }
+
+    public void setPartitsGuanyats(int partitsGuanyats) {
+        this.partitsGuanyats = partitsGuanyats;
+    }
+
+    public int getPartitsPerduts() {
+        return partitsPerduts;
+    }
+
+    public void setPartitsPerduts(int partitsPerduts) {
+        this.partitsPerduts = partitsPerduts;
+    }
+
+    public int getPartitsEmpatats() {
+        return partitsEmpatats;
+    }
+
+    public void setPartitsEmpatats(int partitsEmpatats) {
+        this.partitsEmpatats = partitsEmpatats;
+    }
+
+    public int getPuntsEquip() {
+        return puntsEquip;
+    }
+
+    public void setPuntsEquip(int puntsEquip) {
+        this.puntsEquip = puntsEquip;
+    }
+
+    public int getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(int jornada) {
+        this.jornada = jornada;
+    }
     
+    
+
 }
